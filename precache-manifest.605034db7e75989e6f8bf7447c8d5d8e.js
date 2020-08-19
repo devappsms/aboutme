5c220bae21814e7ddd0699e8bd3482e05b9dd0fa
Mohendren M <mohendren@gmail.com>
2020-08-19 11:37:42 +0530

    Updates

---
+++ precache-manifest.605034db7e75989e6f8bf7447c8d5d8e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b70514df3b97e8936d2e159349eca59",
+    "url": "/%7Baboutme%7D/index.html"
+  },
+  {
+    "revision": "808548c6354805545adc",
+    "url": "/%7Baboutme%7D/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "feba7d85d71d8d8ab4a9",
+    "url": "/%7Baboutme%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "808548c6354805545adc",
+    "url": "/%7Baboutme%7D/static/js/2.6372a5ef.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/%7Baboutme%7D/static/js/2.6372a5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feba7d85d71d8d8ab4a9",
+    "url": "/%7Baboutme%7D/static/js/main.0535b519.chunk.js"
+  },
+  {
+    "revision": "880f5178fc3d96134ce6",
+    "url": "/%7Baboutme%7D/static/js/runtime-main.d71af6bb.js"
+  },
+  {
+    "revision": "48a4c62cd8848aed7c9053eb6e7d4367",
+    "url": "/%7Baboutme%7D/static/media/B.tech.48a4c62c.png"
+  },
+  {
+    "revision": "3212a66c5eb8ba2d7e084cca09accb83",
+    "url": "/%7Baboutme%7D/static/media/MERN-Stack.3212a66c.jpg"
+  },
+  {
+    "revision": "5937a55a85fa2dba114ab4768094a1b8",
+    "url": "/%7Baboutme%7D/static/media/experience.5937a55a.jpg"
+  },
+  {
+    "revision": "61e36bc131c73eb4a6ff90e455b998f6",
+    "url": "/%7Baboutme%7D/static/media/fullStack.61e36bc1.jpg"
+  },
+  {
+    "revision": "f10a3ad142b1c01b14d157381ccf97a1",
+    "url": "/%7Baboutme%7D/static/media/mainframe.f10a3ad1.jpg"
+  }
+]);
